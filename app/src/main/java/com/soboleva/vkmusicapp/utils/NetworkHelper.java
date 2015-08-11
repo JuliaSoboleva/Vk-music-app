f3476dd5ea801c91317862d1ef3cd4a03f96081c
Julia Soboleva <julia.soboleva26@gmail.com>
2015-08-11 23:50:29 +0300

    After git update.

--- app/src/main/java/com/soboleva/vkmusicapp/utils/NetworkHelper.java
+++ app/src/main/java/com/soboleva/vkmusicapp/utils/NetworkHelper.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 
 public class NetworkHelper {
-
-    public static  boolean isNetworkAvailable(Context context) {
+    public static boolean isNetworkAvailable(Context context) {
         return ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo() != null;
     }
 }
